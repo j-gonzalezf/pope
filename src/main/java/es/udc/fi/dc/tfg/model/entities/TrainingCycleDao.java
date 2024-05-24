b77fb4441db4cd244b26411cd94b78a561cf741a
Jorge González Fernández <j.gonzalezf@udc.es>
2024-05-25 01:07:28 +0200

    SCRUM-21 Implementación de la entidad TrainingCycles

---
+++ src/main/java/es/udc/fi/dc/tfg/model/entities/TrainingCycleDao.java
@@ -0,0 +1,9 @@
+package es.udc.fi.dc.tfg.model.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Interfaz TrainingCycleDao.
+ */
+public interface TrainingCycleDao extends JpaRepository<TrainingCycles, Long> {
+}
